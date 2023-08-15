d6cc0880d44d90b022ce94df6a4c3789fc629f54
Sarah <sarahkulsoom18@gmail.com>
2023-08-15 10:38:31 +0530

    Find Middle Of LinkedList

---
+++ FindMiddleOfLL.java
@@ -0,0 +1,12 @@
+public class FindMiddleOfLL {
+    int getMiddle(Node head) {
+        // Your code here.
+        Node slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow.data;
+    }
+}
