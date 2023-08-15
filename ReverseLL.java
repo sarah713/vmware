ed002e2717becfc79a10afa184aaabc9c437d465
Sarah <sarahkulsoom18@gmail.com>
2023-08-15 18:36:07 +0530

    Reverse Linked List

---
+++ ReverseLL.java
@@ -0,0 +1,13 @@
+public class ReverseLL {
+    Node reverseList(Node head) {
+        // code here
+        Node prev = null;
+        while (head != null) {
+            Node next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
