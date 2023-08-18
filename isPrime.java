d916afdc1201350fbbe133b74958c75bc5f3b628
Sarah <sarahkulsoom18@gmail.com>
2023-08-18 12:55:21 +0530

    Is Prime

---
+++ isPrime.java
@@ -0,0 +1,14 @@
+public class isPrime {
+    static int isprime(int N) {
+        if (N <= 1) {
+            return 0;
+        }
+
+        for (int i = 2; i < Math.sqrt(N); i++) {
+            if (N % i == 0) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+}
