503f135c272851573e3f8ed6476e744867f641ef
Sarah <sarahkulsoom18@gmail.com>
2023-08-27 19:21:31 +0530

    Reverse Linkedlist

---
+++ SAP/ReverseLL.java
@@ -0,0 +1,15 @@
+package SAP;
+
+public class ReverseLL {
+    Node reverseList(Node head) {
+        // code here
+        Node prev = null;
+        while (head != null) {
+            Node next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
